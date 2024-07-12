27aca3fec8b8d7814bfdc328e74a7fdb1344ba07
mrcyme <simeonmcl@gmail.com>
2024-07-12 15:41:16 +0200

    Add content type to the POST request in test_ask_route test.

--- test_app.py
+++ test_app.py
@@ -9,7 +9,7 @@ class TestApp(unittest.TestCase):
 
     def test_ask_route(self):
         question = "Is the character a man?"
-        response = self.app.post('/ask', json={'question': question})
+        response = self.app.post('/ask', json={'question': question}, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         data = response.get_json()
         self.assertIsInstance(data, list)
