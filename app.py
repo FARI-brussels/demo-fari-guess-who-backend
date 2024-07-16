dd2bdcbca9d9a059defed453d47cfede08619caf
mrcyme <simeonmcl@gmail.com>
2024-07-16 10:49:06 +0200

    Added SocketIO integration to the Flask app.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
 import openai_api
 import random
 
